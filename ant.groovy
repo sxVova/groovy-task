68481c11febe5e3ebd928bfd64797e78a99fe1c8
sxVova <53950314+sxVova@users.noreply.github.com>
2020-04-15 13:44:44 +0300

    Update ant.groovy

--- ant.groovy
+++ ant.groovy
@@ -22,6 +22,6 @@ job('deploy') {
      * configuring ssh plugin to run docker commands
      */
     steps{
-             shell 'scp /var/lib/jenkins/workspace/antbuild/build/demoapp-dev.war rasavo99@34.90.106.78:/opt/tomcat/latest/webapps/'
+             shell 'scp /var/lib/jenkins/workspace/antbuild/build/demoapp-dev.war root@34.90.106.78:/opt/tomcat/latest/webapps'
       }
 }
